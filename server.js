f6398be904f969b6739e6e7d5665fc2b8a65bba5
HectorJosueDH <hectorjosue78@gmail.com>
2023-08-08 17:06:52 -0600

    Fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'shhhh',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
